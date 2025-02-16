66633f38844820446a95c548df3ad410c7ebd9f9
Alex Yun <iamalexyundev@gmail.com>
2025-02-16 18:27:58 +0900

    finished functions and comments

--- chapter3/functions/src/main.rs
+++ chapter3/functions/src/main.rs
@@ -5,11 +5,40 @@
 // fn another_function(x: i32) {
 //     println!("The value of x is: {x}");
 // }
+// fn main() {
+//     let my_num = 5;
+//     print_labeled_measurement(my_num, 'h');
+// }
+
+// fn print_labeled_measurement(value: u32, unit_label: char) {
+//     println!("The measurement is: {value}{unit_label}");
+// }
+
+// fn main() {
+//     let position = (1, 2);
+//     another_function(position);
+// }
+// //Destructuring
+// fn another_function((x, y): (i32, i32)) {
+//     println!("The value of x and y is: {} and {}", x, y);
+// }
+
+// fn five() -> i32 {
+//     return 5;
+// }
+
+// fn main() {
+//     let x = five();
+
+//     println!("The value of x is: {x}");
+// }
+
 fn main() {
-    let my_num = 5;
-    print_labeled_measurement(my_num, 'h');
+    let x = plus_one(5);
+
+    println!("The value of x is: {x}");
 }
 
-fn print_labeled_measurement(value: u32, unit_label: char) {
-    println!("The measurement is: {value}{unit_label}");
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
