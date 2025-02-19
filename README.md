d8795d1791b83336192d5e7e5c01553001561318
Alex Yun <iamalexyundev@gmail.com>
2025-02-20 01:54:20 +0900

    Finished Chapter 5, starting Chapter 6

--- README.md
+++ README.md
@@ -12,4 +12,4 @@ In Chapter 3, created the following projects:
 
 ## Currently at
 
-Chapter 5.3: Method Syntax
+Chapter 6: Enums and Pattern Matching
--- chapter5/rectangles/src/main.rs
+++ chapter5/rectangles/src/main.rs
@@ -24,15 +24,40 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, rectangle: &Rectangle) -> bool {
+        self.width > rectangle.width && self.height > rectangle.height
+    }
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
-    println!("The are of rectangle is {} square pixels", area(&rect1));
-    println!("rect1 is {:#?}", rect1);
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    let rect4 = Rectangle::square(10);
+    println!("The are of rect1 is {} square pixels", rect1.area());
+    println!("The are of rect2 is {} square pixels", rect2.area());
+    println!("The are of rect3 is {} square pixels", rect3.area());
+    println!("The are of rect4 is {} square pixels", rect4.area());
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("Can rect1 hold rect4? {}", rect1.can_hold(&rect4));
+    println!("Can rect2 hold rect3? {}", rect2.can_hold(&rect3))
 }
