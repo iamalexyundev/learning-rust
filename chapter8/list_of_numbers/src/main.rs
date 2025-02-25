e7a10388409bd6526ccbdc36109a712ef36a7781
Alex Yun <iamalexyundev@gmail.com>
2025-02-25 16:59:26 +0900

    finished mode method on impl of Numbers, maybe will need some refactoring

--- chapter8/list_of_numbers/src/main.rs
+++ chapter8/list_of_numbers/src/main.rs
@@ -11,6 +11,7 @@ impl Numbers {
         }
         self.0.iter().sum::<i32>() as f32 / self.0.len() as f32
     }
+
     fn median(&self) -> f32 {
         if self.0.is_empty() {
             return 0 as f32;
@@ -19,36 +20,36 @@ impl Numbers {
         sorted.sort_unstable();
         let len = sorted.len();
         let is_even = len % 2 == 0;
-        let middle_idx = len / 2;
+        let mid_idx = len / 2;
         if is_even {
-            Numbers(sorted[middle_idx - 1..=middle_idx].to_vec()).mean()
+            Numbers(sorted[mid_idx - 1..=mid_idx].to_vec()).mean()
         } else {
-            sorted[middle_idx] as f32
+            sorted[mid_idx] as f32
         }
+    }
+
+    fn mode(&self) -> i32 {
+        if self.0.is_empty() {
+            return 0;
+        }
+        let mut frequency_map = HashMap::new();
+        let mut most_frequent = (0, 0);
+        for number in &self.0 {
+            let count = frequency_map.entry(number).or_insert(0);
+            *count += 1;
+            if *count > most_frequent.1 {
+                most_frequent = (*number, *count)
+            }
+        }
+        most_frequent.0
     }
 }
 fn main() {
     //Create a list of numbers
     //TODO: Generate list of random number unsorted
-    let list = Numbers(vec![6, 5, 4, 3, 2, 1]);
+    let list = Numbers(vec![5, 4, 3, 2, 1]);
     println!("The mean of the list is {}", list.mean());
     println!("The median of the list is {}", list.median());
-    // println!("The mode of the list is {}", get_mode(&list));
+    println!("The mode of the list is {}", list.mode());
     println!("Initial vector is {:?}", &list);
-}
-
-fn get_mode(list: &[i32]) -> i32 {
-    if list.is_empty() {
-        return 0;
-    }
-    let mut frequency_map = HashMap::new();
-    let mut most_frequent = (0, 0);
-    for number in list {
-        let count = frequency_map.entry(*number).or_insert(0);
-        *count += 1;
-        if *count > most_frequent.1 {
-            most_frequent = (*number, *count)
-        }
-    }
-    most_frequent.0
 }
