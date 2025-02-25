2a020f53065f2cd0422f64e8f3502c75de1d5742
Alex Yun <iamalexyundev@gmail.com>
2025-02-25 15:42:15 +0900

    finished mean and median methods on impl of Numbers

--- chapter8/list_of_numbers/src/main.rs
+++ chapter8/list_of_numbers/src/main.rs
@@ -1,40 +1,40 @@
 use std::collections::HashMap;
 
+#[derive(Debug)]
+struct Numbers(Vec<i32>);
+
+impl Numbers {
+    fn mean(&self) -> f32 {
+        println!("{:?}", self);
+        if self.0.is_empty() {
+            return 0 as f32;
+        }
+        self.0.iter().sum::<i32>() as f32 / self.0.len() as f32
+    }
+    fn median(&self) -> f32 {
+        if self.0.is_empty() {
+            return 0 as f32;
+        }
+        let mut sorted = self.0.to_vec();
+        sorted.sort_unstable();
+        let len = sorted.len();
+        let is_even = len % 2 == 0;
+        let middle_idx = len / 2;
+        if is_even {
+            Numbers(sorted[middle_idx - 1..=middle_idx].to_vec()).mean()
+        } else {
+            sorted[middle_idx] as f32
+        }
+    }
+}
 fn main() {
     //Create a list of numbers
     //TODO: Generate list of random number unsorted
-    let list = vec![1, 2, 3, 4, 5];
-    println!("The mean of the list is {}", get_mean(&list));
-    println!("The median of the list is {}", get_median(&list[..])); //&list is pretty much the same
-    println!("The mode of the list is {}", get_mode(&list));
+    let list = Numbers(vec![6, 5, 4, 3, 2, 1]);
+    println!("The mean of the list is {}", list.mean());
+    println!("The median of the list is {}", list.median());
+    // println!("The mode of the list is {}", get_mode(&list));
     println!("Initial vector is {:?}", &list);
-}
-
-fn get_mean(list: &[i32]) -> f32 {
-    if list.is_empty() {
-        return 0.0;
-    }
-    let mut total: f32 = 0.0;
-    for num in list {
-        total += *num as f32
-    }
-    total / list.len() as f32
-}
-
-//&[i32] because Rust converts it into slice automatically
-//it is better because this function can also work with slices of type i32
-//arrays etc. it makes it more flexible by not accepting only Vec<i32>
-fn get_median(list: &[i32]) -> f32 {
-    let mut sorted = list.to_vec();
-    let length = sorted.len();
-    let is_even = length % 2 == 0;
-    sorted.sort_unstable();
-    let index = length / 2;
-    if is_even {
-        get_mean(&sorted[index - 1..=index])
-    } else {
-        sorted[index] as f32
-    }
 }
 
 fn get_mode(list: &[i32]) -> i32 {
