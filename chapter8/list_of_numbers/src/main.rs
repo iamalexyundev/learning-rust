341fbd44201d7ccf9912eb7b6106523b58c96bac
Alex Yun <iamalexyundev@gmail.com>
2025-02-26 13:45:22 +0900

    refactored list of numbers

--- chapter8/list_of_numbers/src/main.rs
+++ chapter8/list_of_numbers/src/main.rs
@@ -5,7 +5,6 @@ struct Numbers(Vec<i32>);
 
 impl Numbers {
     fn mean(&self) -> f32 {
-        println!("{:?}", self);
         if self.0.is_empty() {
             return 0 as f32;
         }
@@ -16,40 +15,40 @@ impl Numbers {
         if self.0.is_empty() {
             return 0 as f32;
         }
-        let mut sorted = self.0.to_vec();
+        let mut sorted = self.0.clone();
         sorted.sort_unstable();
+
         let len = sorted.len();
-        let is_even = len % 2 == 0;
         let mid_idx = len / 2;
-        if is_even {
-            Numbers(sorted[mid_idx - 1..=mid_idx].to_vec()).mean()
-        } else {
-            sorted[mid_idx] as f32
+
+        match sorted.len() % 2 {
+            0 => (sorted[mid_idx - 1] + sorted[mid_idx]) as f32 / 2.0,
+            _ => sorted[mid_idx] as f32,
         }
     }
 
-    fn mode(&self) -> i32 {
+    fn mode(&self) -> Option<i32> {
         if self.0.is_empty() {
-            return 0;
+            return None;
         }
         let mut frequency_map = HashMap::new();
-        let mut most_frequent = (0, 0);
-        for number in &self.0 {
-            let count = frequency_map.entry(number).or_insert(0);
-            *count += 1;
-            if *count > most_frequent.1 {
-                most_frequent = (*number, *count)
-            }
-        }
-        most_frequent.0
+
+        self.0.iter().for_each(|&num| {
+            *frequency_map.entry(num).or_insert(0) += 1;
+        });
+
+        frequency_map
+            .into_iter()
+            .max_by_key(|&(_, count)| count)
+            .map(|(num, _)| num)
     }
 }
 fn main() {
     //Create a list of numbers
     //TODO: Generate list of random number unsorted
-    let list = Numbers(vec![5, 4, 3, 2, 1]);
+    let list = Numbers(vec![5, 4, 3, 2, 1, 5, 5, 5]);
     println!("The mean of the list is {}", list.mean());
     println!("The median of the list is {}", list.median());
-    println!("The mode of the list is {}", list.mode());
+    println!("The mode of the list is {}", list.mode().unwrap());
     println!("Initial vector is {:?}", &list);
 }
