bf87c9e8f60f514f50c04417b789f65f9c79a66b
Alex Yun <iamalexyundev@gmail.com>
2025-03-01 15:18:30 +0500

    still working on department lookup

--- chapter8/department_lookup/src/main.rs
+++ chapter8/department_lookup/src/main.rs
@@ -1,6 +1,67 @@
 use std::collections::HashMap;
-fn main() {
-    let mut departments = HashMap::new();
-    departments.insert("Engineering".to_string(), "Alex".to_string());
-    println!("{:?}", departments)
+use std::io;
+#[derive(Debug)]
+enum Command {
+    Add {
+        names: Vec<String>,
+        department: String,
+    },
+    Get(String),
+    GetAll,
+    Remove {
+        names: Vec<String>,
+        department: String,
+    },
+    Quit,
 }
+impl Command {
+    fn parse_command(command_parts: &Vec<&str>) -> Option<Command> {
+        let action = command_parts[0];
+        match action {
+            "add" => Some(Command::Add {
+                names: vec![command_parts[1].to_owned()],
+                department: command_parts[command_parts.len() - 1].to_owned(),
+            }),
+            "get" => None,
+            _ => {
+                println!("Unknown command");
+                None
+            }
+        }
+    }
+}
+
+fn main() {
+    println!("Welcome to the Department Lookup!");
+    println!("What would you like to do? [add, get, or remove] employee(s)");
+    let mut departments: HashMap<String, Vec<String>> = HashMap::new();
+    loop {
+        let raw_input = get_raw_input();
+        let command_parts: Vec<&str> = raw_input.split_whitespace().collect();
+        let command = Command::parse_command(&command_parts);
+        // let command = Command::parse_command(&command_parts);
+        // let action = Command::get_action(command_parts[0]);
+        // if action.is_none() {
+        //     continue;
+        // }
+        // if let Some(cmd) = action {
+        //     match cmd {
+        //         Command::Add => add(&mut departments),
+        //         Command::Get => get(&departments),
+        //         Command::Remove => remove(&mut departments),
+        //     }
+        // }
+    }
+}
+
+fn get_raw_input() -> String {
+    let mut buffer = String::new();
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Failed to get the input");
+    buffer.trim().to_owned()
+}
+
+fn add(departments: &mut HashMap<String, Vec<String>>) {}
+fn get(departments: &HashMap<String, Vec<String>>) {}
+fn remove(departments: &mut HashMap<String, Vec<String>>) {}
