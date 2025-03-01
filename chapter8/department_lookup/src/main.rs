b9559db6c86bcf8c587466ff8fa2e8493468cc6c
Alex Yun <iamalexyundev@gmail.com>
2025-03-01 20:02:42 +0500

    added add function

--- chapter8/department_lookup/src/main.rs
+++ chapter8/department_lookup/src/main.rs
@@ -2,27 +2,18 @@ use std::collections::HashMap;
 use std::io;
 #[derive(Debug)]
 enum Command {
-    Add {
-        names: Vec<String>,
-        department: String,
-    },
+    Add { name: String, department: String },
     Get(String),
     GetAll,
-    Remove {
-        names: Vec<String>,
-        department: String,
-    },
     Quit,
 }
 impl Command {
-    fn parse_command(command_parts: &Vec<&str>) -> Option<Command> {
-        let action = command_parts[0];
-        match action {
-            "add" => Some(Command::Add {
-                names: vec![command_parts[1].to_owned()],
-                department: command_parts[command_parts.len() - 1].to_owned(),
-            }),
-            "get" => None,
+    fn parse_command(command_parts: Vec<&str>) -> Option<Command> {
+        let action = command_parts[0].to_lowercase();
+        match action.as_str() {
+            "add" => parse_add(&command_parts),
+            "get" => parse_get(&command_parts),
+            "q" | "quit" => Some(Command::Quit),
             _ => {
                 println!("Unknown command");
                 None
@@ -33,24 +24,22 @@ impl Command {
 
 fn main() {
     println!("Welcome to the Department Lookup!");
-    println!("What would you like to do? [add, get, or remove] employee(s)");
     let mut departments: HashMap<String, Vec<String>> = HashMap::new();
     loop {
+        println!("What would you like to do? [add, get, or remove] employee(s)");
         let raw_input = get_raw_input();
         let command_parts: Vec<&str> = raw_input.split_whitespace().collect();
-        let command = Command::parse_command(&command_parts);
-        // let command = Command::parse_command(&command_parts);
-        // let action = Command::get_action(command_parts[0]);
-        // if action.is_none() {
-        //     continue;
-        // }
-        // if let Some(cmd) = action {
-        //     match cmd {
-        //         Command::Add => add(&mut departments),
-        //         Command::Get => get(&departments),
-        //         Command::Remove => remove(&mut departments),
-        //     }
-        // }
+        let command = Command::parse_command(command_parts);
+        if command.is_none() {
+            continue;
+        }
+        match command.unwrap() {
+            Command::Add { name, department } => {
+                departments.entry(department).or_default().push(name);
+            }
+            Command::Quit => std::process::exit(1),
+            _ => todo!(),
+        }
     }
 }
 
@@ -62,6 +51,17 @@ fn get_raw_input() -> String {
     buffer.trim().to_owned()
 }
 
-fn add(departments: &mut HashMap<String, Vec<String>>) {}
-fn get(departments: &HashMap<String, Vec<String>>) {}
-fn remove(departments: &mut HashMap<String, Vec<String>>) {}
+fn parse_add(command_parts: &Vec<&str>) -> Option<Command> {
+    if command_parts.len() < 4 || !command_parts.contains(&"to") || command_parts.len() > 4 {
+        println!("Wrong format! Usage: add name to department");
+        None
+    } else {
+        let name = command_parts[1].to_owned();
+        let department = command_parts[3].to_owned();
+        Some(Command::Add { name, department })
+    }
+}
+
+fn parse_get(command_parts: &Vec<&str>) -> Option<Command> {
+    Some(Command::Get("GWET".to_owned()))
+}
