6fbe34883e755b3b71266d5883f52962528bb0fa
Alex Yun <iamalexyundev@gmail.com>
2025-03-04 23:52:06 +0500

    lifetimes of struct

--- chapter10/lifetimes/src/main.rs
+++ chapter10/lifetimes/src/main.rs
@@ -1,3 +1,8 @@
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
 fn main() {
     let string1 = String::from("abcd");
     let result;
@@ -6,6 +11,12 @@ fn main() {
         result = longest(&string1, &string2);
         println!("the longest string is {result}");
     }
+
+    let sentence = String::from("This is the first part. This is the second part.");
+    let first_part = sentence.split(".").next().expect("Could not find a '.'");
+    let excerpt = ImportantExcerpt { part: first_part };
+
+    println!("The excerpt is: {:?}", excerpt.part);
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
