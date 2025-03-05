d1c24648e27e9097682d9f83a004b4772c2fcd4a
Alex Yun <iamalexyundev@gmail.com>
2025-03-05 16:44:34 +0500

    starting minigrep project

---
+++ chapter12/minigrep/src/main.rs
@@ -0,0 +1,37 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = parse_config(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    println!("With text:\n{contents}");
+}
+
+//more efficient with lifetimes
+struct Config<'a> {
+    query: &'a str,
+    file_path: &'a str,
+}
+fn parse_config(args: &[String]) -> Config {
+    Config {
+        query: &args[1],
+        file_path: &args[2],
+    }
+}
+//inefficient way
+// struct Config {
+//     query: String,
+//     file_path: String,
+// }
+// fn parse_config(args: &[String]) -> Config {
+//     Config {
+//         query: args[1].clone(),
+//         file_path: args[2].clone(),
+//     }
+// }
