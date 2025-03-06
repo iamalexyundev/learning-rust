0392949a55dfdf855a000be38630d12aa433de9d
Alex Yun <iamalexyundev@gmail.com>
2025-03-06 19:02:16 +0500

    Finished Chapter 12, starting Chapter 13

--- README.md
+++ README.md
@@ -22,4 +22,4 @@ In Chapter 8, created the following projects:
 
 ## Currently at
 
-Chapter 12.5: Working with Environment Variables
+Chapter 13: Functional Language Features: Iterators and Closures
--- chapter12/minigrep/src/main.rs
+++ chapter12/minigrep/src/main.rs
@@ -4,12 +4,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments:\n{err}");
+        eprintln!("Problem parsing arguments:\n{err}");
         process::exit(1)
     });
 
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(2)
     }
 }
